1d5e350d9dc764fc6b11f68fb6b07facb3d07ba4
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-07 22:20:13 +0700

    use api

--- index_readApi.js
+++ index_readApi.js
@@ -17,6 +17,26 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb://MyUserAdmin:MyUserAdmin@localhost:27017";
 
 
+app.post('/users/create', async(req, res) => {
+    const user = req.body;
+    const client = new MongoClient(uri);
+    await client.connect();
+    await client.db('mydb').collection('users').insertOne({
+        id: parseInt(user.id),
+        fname: user.fname,
+        lname: user.lname,
+        username: user.username,
+        email: user.email,
+        avatar: user.avatar
+    });
+    await client.close();
+    res.status(200).send({
+        "status": "ok",
+        "message": "User with ID = " + user.id + " is created",
+        "user": user
+    });
+
+})
 
 app.get('/users', async(req, res) => {
 
@@ -42,7 +62,7 @@ app.get('/users/:id', async(req, res) => {
 
 app.put('/users/update', async(req, res) => {
     const user = req.body;
-    const id = user.id;
+    const id = parseInt(user.id);
     const client = new MongoClient(uri);
     await client.connect();
     await client.db('mydb').collection('users').updateOne({ "id": id }, {
@@ -65,7 +85,7 @@ app.put('/users/update', async(req, res) => {
 
 app.delete('/users/delete', async(req, res) => {
 
-    const id = req.body.id;
+    const id = parseInt(req.body.id);
     const client = new MongoClient(uri);
     await client.connect();
     await client.db('mydb').collection('users').deleteOne({ "id": id });
