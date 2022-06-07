2552ec2d72743f63ea72d721dc839222858c34ff
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-07 18:55:36 +0700

    commint get data

---
+++ index_read.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('aom come use nodemon')
+
+})
+app.listen(port, () => {
+    console.log('example app listen in port' + port)
+})
+
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://MyUserAdmin:MyUserAdmin@localhost:27017";
+
+app.get('/users', async(req, res) => {
+
+    const client = new MongoClient(uri);
+    await client.connect();
+    await client.db('mydb').collection('users').find({}).toArray();
+    await client.close();
+    res.status(200).send(users)
+
+})
